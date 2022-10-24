c99325e4852a67e2b7467c31fe12037a050c88c6
Dante Araújo <danteeng@hotmail.com>
2022-10-24 18:41:19 -0300

    Installing mongoose node_module and making our first connectiong with mongoDb

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,24 @@
+require("dotenv").config();
 const express = require ("express");
+const mongoose = require('mongoose');
 const app = express();
 
+// Config constants
 const PORT = 3000;
-const API_URL_PATH = "/api/v1"
+const API_URL_PATH = "/api/v1";
+const DB_USER = process.env.DB_USER;
+const DB_PASS = encodeURIComponent(process.env.DB_PASSWORD);
 
-const json = require('./dataa.json')
+const json = require('./dataa.json');
+
+mongoose
+  .connect(`mongodb+srv://${DB_USER}:${DB_PASS}@cluster-00.8nwrqdh.mongodb.net/?retryWrites=true&w=majority`)
+  .then(() => {
+    console.log("Conectamos ao MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // I Have the ability to create routes
 // ROUTES
