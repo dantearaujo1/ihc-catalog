ead1ae86657daaa4b0bbdb3e3559a21487f39e96
Dante Araújo <danteeng@hotmail.com>
2022-10-24 21:22:57 -0300

    Adding middleware for parsing data in req.body for json and urlencoded and integrated our app.js with our routing

--- backend/app.js
+++ backend/app.js
@@ -3,14 +3,21 @@ const express = require ("express");
 const mongoose = require('mongoose');
 const app = express();
 
+// This parses incoming JSON requests and puts the parsed data in req.body
+app.use(express.json());
+// Body parser for html post fomr
+app.use(express.urlencoded({extended:false}));
+
 // Config constants
 const PORT = 3000;
 const API_URL_PATH = "/api/v1";
 const DB_USER = process.env.DB_USER;
 const DB_PASS = encodeURIComponent(process.env.DB_PASSWORD);
 
+// Local data
 const json = require('./dataa.json');
 
+// Connection between database and the server application
 mongoose
   .connect(`mongodb+srv://${DB_USER}:${DB_PASS}@cluster-00.8nwrqdh.mongodb.net/?retryWrites=true&w=majority`)
   .then(() => {
@@ -20,14 +27,15 @@ mongoose
     console.log(err);
   });
 
+
 // I Have the ability to create routes
 // ROUTES
+const articleRoutes = require('./routes/articleRoutes');
+app.use(API_URL_PATH.concat('/','article'), articleRoutes);
+
+// TODO: Remove this! Is for testing purpose only
 app.get(API_URL_PATH, (req,res) => {
   res.json(json);
-});
-
-app.get(API_URL_PATH + '/posts', (req,res) => {
-  res.send("We are on posts");
 });
 
 
