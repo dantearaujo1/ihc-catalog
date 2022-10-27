df0b4ea50235c5b7b87e659b1ead0fc374e031bb
Dante Araújo <danteeng@hotmail.com>
2022-10-27 20:29:04 -0300

    Correcting formatting

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,84 +1,134 @@
-import React from 'react';
-import TagSelect from '../components/Filter/TagSelect.jsx'
-import NavigationHeader from '../components/Navigation/NavigationHeader'
+import React from "react";
+import TagSelect from "../components/Filter/TagSelect.jsx";
+import NavigationHeader from "../components/Navigation/NavigationHeader";
 
-import Typography from '@mui/material/Typography';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 function Home() {
-
   const [data, setData] = useState([]);
-  const [filtered, setFiltered] = useState('');
+  const [filtered, setFiltered] = useState("");
   const [data_filtered, setDataFiltered] = useState();
-
 
   // Getting data from the backend
   // WARN: we should use useQuery hook for this
-  useEffect( ()=>{
-
-    const fetch_data = async() => {
-      const data = await fetch('/api/v1');
+  useEffect(() => {
+    const fetch_data = async () => {
+      const data = await fetch("/api/v1");
       const json = await data.json();
       // Setting data to data using useState
       setData(json.complete_detailed_list);
-    }
+    };
     fetch_data().catch(console.error);
-
   }, [filtered]);
 
   // This will work as our searchBar filter
-  useEffect( ()  => {
+  useEffect(() => {
     // Check if search bar is not Empty
-    if(filtered !== ""){
+    if (filtered !== "") {
       //Setting our data_filtered to only contains what is
       // in the searchBox
-      setDataFiltered(data.filter((article)=>{
-        if(article.ux_instruments){
-          return article.ux_instruments.toLowerCase().includes(filtered.toLowerCase());
-        }
-      }));
+      setDataFiltered(
+        data.filter((article) => {
+          if (article.ux_instruments) {
+            return article.ux_instruments
+              .toLowerCase()
+              .includes(filtered.toLowerCase());
+          }
+        })
+      );
     }
     // if searchBar is empty we should set our full data to the list
-    else{
+    else {
       // checking if we already have some data
-      if(data.lenght !== 0){
+      if (data.lenght !== 0) {
         setDataFiltered([]);
       }
     }
-  }, [filtered] ); /* This last parameters means that we gonna call useEffect
+  }, [filtered]); /* This last parameters means that we gonna call useEffect
                       Everytime that filtered is diferrent;
   */
-  return(
+  return (
     <div>
-      <NavigationHeader Filter={setFiltered} data={data_filtered?data_filtered:data} isAdmin={false}>
-      </NavigationHeader>
+      <NavigationHeader
+        Filter={setFiltered}
+        data={data_filtered ? data_filtered : data}
+        isAdmin={false}
+      ></NavigationHeader>
       <div className="title-container">
-        <Typography variant="h1" sx={{marginLeft: 30, marginTop: 10}}>
+        <Typography variant="h1" sx={{ marginLeft: 30, marginTop: 10 }}>
           This is Human Computer Interaction
         </Typography>
-        <Typography variant="h3" color='#505050' sx={{marginLeft: 30, marginTop: 2}}>
+        <Typography
+          variant="h3"
+          color="#505050"
+          sx={{ marginLeft: 30, marginTop: 2 }}
+        >
           Find the better Instrument for you project
         </Typography>
       </div>
-      <Stack direction="row" sx={{flexWrap: "wrap", justifyContent:"center", marginTop: 5}}>
-          <TagSelect data={["Children", "Older", "All type of Users", "Jornalists", "Consumers","People with disabilities", "Role-Specific"].sort()} placeHolder={"Target"}></TagSelect>
-          <TagSelect data={["Application-independent", "Online Plataform", "Mobile","Audiovisual"].sort()} placeHolder={"Domain"}></TagSelect>
-          <TagSelect data={["Questionary", "Two-dimensional Diagrams/Graphs area","Scale","Pshychophysiology","Post-test Picture/Object",].sort()} placeHolder={"Type"}></TagSelect>
-          <TagSelect data={["Qualitative", "Quantitative", "Quali-Quantitative"].sort()} placeHolder={"Approach"}></TagSelect>
-          <TagSelect data={["Aesthetics", "Affect", "Appreaisal","Aspects of Game Experience"].sort()} placeHolder={"Quality UX"}></TagSelect>
+      <Stack
+        direction="row"
+        sx={{ flexWrap: "wrap", justifyContent: "center", marginTop: 5 }}
+      >
+        <TagSelect
+          data={[
+            "Children",
+            "Older",
+            "All type of Users",
+            "Jornalists",
+            "Consumers",
+            "People with disabilities",
+            "Role-Specific",
+          ].sort()}
+          placeHolder={"Target"}
+        ></TagSelect>
+        <TagSelect
+          data={[
+            "Application-independent",
+            "Online Plataform",
+            "Mobile",
+            "Audiovisual",
+          ].sort()}
+          placeHolder={"Domain"}
+        ></TagSelect>
+        <TagSelect
+          data={[
+            "Questionary",
+            "Two-dimensional Diagrams/Graphs area",
+            "Scale",
+            "Pshychophysiology",
+            "Post-test Picture/Object",
+          ].sort()}
+          placeHolder={"Type"}
+        ></TagSelect>
+        <TagSelect
+          data={["Qualitative", "Quantitative", "Quali-Quantitative"].sort()}
+          placeHolder={"Approach"}
+        ></TagSelect>
+        <TagSelect
+          data={[
+            "Aesthetics",
+            "Affect",
+            "Appreaisal",
+            "Aspects of Game Experience",
+          ].sort()}
+          placeHolder={"Quality UX"}
+        ></TagSelect>
       </Stack>
-      <Stack sx={{alignItems:"center", marginTop: 5}}>
+      <Stack sx={{ alignItems: "center", marginTop: 5 }}>
         <Button
-        color='secondary'
-        variant="contained"
-        sx={{width:0.2, height: "3rem", borderRadius: 50}}
-        startIcon={<FontAwesomeIcon icon={faMagnifyingGlass}/>}>
+          color="secondary"
+          variant="contained"
+          sx={{ width: 0.2, height: "3rem", borderRadius: 50 }}
+          startIcon={<FontAwesomeIcon icon={faMagnifyingGlass} />}
+        >
           Filter
         </Button>
       </Stack>
@@ -86,4 +136,4 @@ function Home() {
   );
 }
 
-export default Home
+export default Home;
--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,99 +1,97 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import NavigationHeader from '../components/Navigation/NavigationHeader';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
-
-function Login(){
-
+function Login() {
   const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
   };
 
   return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            padding: 3,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            backgroundColor: '#FFFFFF',
-            borderRadius: '10px'
-          }}
-        >
-          <Avatar sx={{ m: 2, bgcolor: '#000000' }}/>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={() => navigate('/dboard_admin')}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          padding: 3,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          backgroundColor: "#FFFFFF",
+          borderRadius: "10px",
+        }}
+      >
+        <Avatar sx={{ m: 2, bgcolor: "#000000" }} />
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            onClick={() => navigate("/dboard_admin")}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
             </Grid>
-          </Box>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
         </Box>
-      </Container>
-  )}
+      </Box>
+    </Container>
+  );
+}
 
 export default Login;
