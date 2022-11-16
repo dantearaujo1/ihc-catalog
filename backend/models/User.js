ea2c914b40376092da1ecef96de37b56dccb090c
Dante Araújo <danteeng@hotmail.com>
2022-11-16 12:26:19 -0300

    Create better schemes for articles, groups and users

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,41 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const User = mongoose.model('User', {
-  username: String,
-  password: String,
-  email: String,
-  role: String,
-  contents: Object
+const userSchema = new mongoose.Schema({
+  uname:{
+    type:String,
+    required:true,
+    trim:true,
+    unique:true,
+  },
+  email:{
+    type:String,
+    required:true,
+    unique:true
+  },
+  password:{
+    type:String,
+    required:true,
+    minlenght:6
+  },
+  tokens: [
+    {
+      token:{
+        type:String,
+        required:true,
+      }
+    }
+ ],
 })
 
-module.exports = User
+
+// Hashing password
+userSchema.pre("save", async function(next){
+  this.password = await bcrypt.hash(this.password,12);
+  next()
+})
+
+// Creating the model
+const UserDB = mongoose.model('Users', userSchema)
+
+module.exports = UserDB
