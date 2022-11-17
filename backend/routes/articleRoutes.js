15796cf4bc7698bbd8fcf2d29a2ba7dc1e686e4d
Dante Araújo <danteeng@hotmail.com>
2022-11-17 09:46:27 -0300

    Remove functions from routes and put them into controller file | Create function to batch insert from local to remote db

--- backend/routes/articleRoutes.js
+++ backend/routes/articleRoutes.js
@@ -1,98 +1,24 @@
 const router = require('express').Router();
-const Article = require('../models/Article');
+const { createArticle, getArticleById, getArticles, patchArticle, deleteArticle, sendToDatabase} = require('../controllers/articleController');
 
 // Adding an article to database
 // TODO: we need to add a group to the article and their categories
 // TODO: we need to add main idea property and general procedure
-router.post('/', async (req,res) => {
+router.post('/', createArticle);
 
-  const {name,reference,year,type} = req.body;
-  const article =  {
-    name,
-    reference,
-    year,
-    type
-  }
-
-  if(!name) {
-    res.status(422).json({error: 'O nome é obrigatório'})
-    return;
-  }
-
-  try {
-    await Article.create(article);
-
-    res.status(201).json({message: 'Artigo inserido com sucesso'});
-
-  } catch (error) {
-     res.status(500).json({error:error});
-  }
-
-});
+// WARN: This is for sending from local to remoteDatabase
+// Dont mess with this if u don't know
+// router.post('/add', sendToDatabase);
 
 // Recieving all articles stored
-router.get('/', async (req, res) => {
-  try {
-    const articles = await Article.find();
-    res.status(200).json(articles);
-
-  } catch (error) {
-     res.status(500).json({error:error});
-  }
-})
+router.get('/', getArticles);
 
 // Recieving an article by id
-router.get('/:id', async (req,res) => {
-  const id = req.params.id;
-  try {
-
-    const article = await Article.findOne({_id: id});
-    if(!article){
-      res.status(422).json({message: "O usuários não foi encontrado"});
-      return;
-    }
-    res.status(200).json(article);
-
-  } catch {
-     res.status(500).json({error:error});
-  }
-})
+router.get('/:id', getArticleById);
 
 // Updating our articles data (PUT -- Update all the article, PATCH update part of an article)
-router.patch('/:id', async (req,res) => {
-  const id = req.params.id;
-  const {name,reference,year,type} = req.body;
-  const article =  {
-    name, reference, year, type
-  }
-
-  try {
-
-    const updatedArticle = await Article.updateOne({ _id:id } ,article);
-    if ( updatedArticle.matchedCount === 0 ){
-      res.status(422).json({message:"Usuário não encontrado!"})
-    }
-    res.status(200).json(updatedArticle);
-
-  } catch {
-     res.status(500).json({error:error});
-  }
-})
+router.patch('/:id', patchArticle)
 
 // Delete an article by id
-router.delete('/:id', async (req,res) => {
-  const id = req.params.id;
-  const article = await Article.findOne({_id: id});
-  if(!article){
-    res.status(422).json({message: "O usuários não foi encontrado!"});
-    return;
-  }
-  try {
-    await Article.deleteOne({_id:id});
-    res.status(200).json({message:'Usuário removido com sucesso!'});
-
-  } catch {
-     res.status(500).json({error:error});
-  }
-})
+router.delete('/:id', deleteArticle)
 module.exports = router;
