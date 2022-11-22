46500a1a9caeb9c7963011976bac9b69088dd8af
Dante Araújo <danteeng@hotmail.com>
2022-11-22 15:42:42 -0300

    Passing data to the article from props

--- frontend/src/components/ArticleCard.jsx
+++ frontend/src/components/ArticleCard.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
 import Box from "@mui/material/Box"
 import Button from "@mui/material/Button"
 import Typography from "@mui/material/Typography"
@@ -9,25 +10,29 @@ import CardContent from "@mui/material/CardContent"
 import CardActions from "@mui/material/CardActions"
 
 const ArticleCard = (props) => {
-  const [data, setData] = useState({});
+  const [data, setData] = useState(props.data);
+  const navigate = useNavigate();
+  const handleClick = (e,value) => {
+    navigate('../../instrument_detail');
+  }
   return (
     <Box>
       <Stack alignItems="center">
         <Card  sx={{maxWidth: 1000, width: "auto", height: "auto", mr:props.mr, ml:props.ml, borderRadius: 10, mb: 4, mt: 4}} >
           <CardHeader
-            title={data.title?data.title:"Title of the Instrument"}
-            subheader={data.subheader?data.subheader:"Subheader"}
+            title={data?data.Article.name:"Title of the Instrument"}
+            subheader={data?data.Article.reference:"Subheader"}
             sx={{ml:5, mt:2, }}
           />
           <CardContent
             sx={{ml:5, }}
           >
             <Typography variant="h6">
-              {data.content?data.content:"Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."}
+              {data?data.Article.main:"Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."}
             </Typography>
           </CardContent>
           <CardActions disableSpacing>
-            <Button variant="contained" sx={{borderRadius: 10, ml:"auto", mr: 10, mb: 4 }}>
+            <Button onClick={handleClick} variant="contained" sx={{borderRadius: 10, ml:"auto", mr: 10, mb: 4 }}>
               <Typography>Read more</Typography>
             </Button>
           </CardActions>
