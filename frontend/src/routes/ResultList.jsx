5800b2fbb4f02658c085894c96f42898a17bd87c
Dante Araújo <danteeng@hotmail.com>
2022-11-22 08:41:21 -0300

    Passing params to result list and passing empty data to navigationHeader

--- frontend/src/routes/ResultList.jsx
+++ frontend/src/routes/ResultList.jsx
@@ -1,10 +1,12 @@
-import React, {useState} from "react";
-import Pagination from "@mui/material/Pagination"
-import Stack from "@mui/material/Stack"
-import Box from "@mui/material/Box"
-import Button from "@mui/material/Button"
-import Typography from "@mui/material/Typography"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, {useState, useEffect} from "react";
+import { useParams } from "react-router-dom";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";;
 import {
   faArrowLeftLong,
 } from "@fortawesome/free-solid-svg-icons";
@@ -13,12 +15,26 @@ import NavigationHeader from "../components/Navigation/NavigationHeader"
 import NavigationBar from "../components/Navigation/NavigationBar"
 import ArticleCard from "../components/ArticleCard"
 
-const ResultList = () => {
+export default function ResultList() {
   const [page, setPage] = useState(5);
   const [found, setFound] = useState(true);
+  const [result, setResult] = useState({});
+  let {subID} = useParams();
+
+  // This need to fetch article Data
+  useEffect( () => {
+    const fetch_data = async () => {
+      const data = await fetch('/api/v1/article/sub/all')
+      const json = await data.json();
+      console.log(json);
+    }
+    fetch_data();
+    console.log(subID);
+  }, [] );
+
   return (
     <Stack>
-      <NavigationHeader></NavigationHeader>
+      <NavigationHeader data={result?[]:[]}></NavigationHeader>
       <NavigationBar></NavigationBar>
       <Stack sx={{mt:3}} justifyContent="center" alignItems="center">
         {found?
@@ -47,4 +63,3 @@ const ResultList = () => {
   )
 }
 
-export default ResultList;
