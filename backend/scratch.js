96045c86e26d5a2d3bfc5d1ce8a887644835fb9b
Dante Araújo <danteeng@hotmail.com>
2022-11-22 08:38:11 -0300

    Test file to run with nodejs

--- backend/scratch.js
+++ backend/scratch.js
@@ -1,6 +1,9 @@
-const json = require('./dataa.json')
+const json = require('./dataa.json');
+const fs = require('fs');
 const list = json.complete_detailed_list;
+const Categories = ['ux-quality','type_of_approach', 'application_domain', 'type_of_instrument', 'target_users_', 'framework'];
 
+// Try to sanitize categories values to be distinct
 const getDistinctValues = (data , category) => {
 
   const arrayOfQualities = data.flatMap( (item, index) => {
@@ -26,12 +29,94 @@ const createObject = (categoryName, list) => {
   })
 }
 
+// Creates an object with name and a random color from a list
+const createCategory = (list) => {
+  const categories = [];
+  const categoryString = '';
+  list.map( (value) => {
+    const category = {
+      name:value.split('_').reduce((categoryString, word, i) => { return (categoryString + ' ' + word) }).trim(),
+      color:'#' + Math.floor(Math.random()*16777215).toString(16)
+    }
+    categories.push(category);
 
-// console.log(createObject('Quality UX', getDistinctValues(list,'ux-quality')));
-// console.log(createObject('Target Users', getDistinctValues(list,'type_of_approach')));
+  } )
+  return categories;
+}
 
-// console.log(getDistinctValues(list,'type_of_approach'));
-// console.log(getDistinctValues(list,'application_domain'));
-// console.log(getDistinctValues(list,'type_of_instrument'));
-// console.log(getDistinctValues(list,'target_users_'));
-// console.log(getDistincValues(list,'framework'));
+// Correct string with "-" instead of " "
+const adjustCategory = (array) => {
+  const correct = '';
+  return array.map((item) => { return item.name.split('-').reduce((correct,word,i) => { return (correct + ' ' + word)}) });
+}
+
+// Make first Letter of any word Capitalized
+// must recieve an array of strings
+const capitalizeCategories = (array) => {
+  const capitalized = '';
+  return array.map( (category) => { return category.split(' ').reduce((capitalized,word,i) => { return (capitalized[0].toUpperCase() + capitalized.slice(1) + ' ' + word[0].toUpperCase() + word.slice(1)) }) } );
+}
+
+
+
+const objectCategories = createCategory(Categories);
+const noUndescoreCategories = adjustCategory(objectCategories);
+const capitalizedCategories = capitalizeCategories(noUndescoreCategories);
+const allCombinations = Categories.map( (category) => {
+  const result = capitalizedCategories.map( (capitalized) => {
+      const result2 = createObject(capitalized,getDistinctValues(list,category));
+      return result2;
+  } );
+  return result;
+})
+
+// Get all subcategories based on a list of Categories
+// Must have the correct order from Categories list
+const getSubCategoriesByIndex = (index) => {
+  return allCombinations.map((value) => {  return value[index]; })[index];
+}
+
+
+const allSubCategoriesObjects = Categories.map( (value,index) => { return getSubCategoriesByIndex(index);  } );
+
+
+// console.log(getDistinctValues(list,'ux-quality'))
+// console.log(allSubCategoriesObjects[0]);
+
+const sanitezed = Categories.map( (value, index) => { return allSubCategoriesObjects[index] } )
+const jsonSanitezed = JSON.stringify(sanitezed);
+
+// console.log(sanitezed.map((value, index) => {
+//  return value.length;
+// }));
+// Creating a file with sanitized data
+// fs.writeFile('sanitized_data.json', jsonSanitezed, (err,result) => { if(err) console.log(result) });
+
+// Trying to create Group Object
+// console.log(capitalizedCategories.sort());
+const articleXsub = list.map(
+  ( article ) => {
+    return {
+      name:article.ux_instruments,
+      c1:article.type_of_instrument,
+      c2:article.type_of_approach,
+      c3:article.application_domain,
+      c4:article['ux-quality'],
+      c5:article.target_users_,
+      c6:article.framework,
+    }
+  })
+
+console.log(allSubCategoriesObjects);
+
+const articleXsubJSON = JSON.stringify(articleXsub);
+// fs.writeFile('articleXsub.json', articleXsubJSON, (err,result) => { if(err) console.log(result) });
+
+// c1 type of instrument
+// c2 type of approach
+// c3 application domain
+// c4 ux quality
+// c5 target users
+// c6 framework
+
+
