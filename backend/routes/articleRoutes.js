f2d0f0d601f00651ad3623f0cba82065bee49ea1
Dante Araújo <danteeng@hotmail.com>
2022-11-22 08:40:38 -0300

    Adding getArticleById and by Name, do not use sendGroupToDatabase

--- backend/routes/articleRoutes.js
+++ backend/routes/articleRoutes.js
@@ -1,20 +1,26 @@
 const router = require('express').Router();
-const { createArticle, getArticleById, getArticles, patchArticle, deleteArticle, sendToDatabase} = require('../controllers/articleController');
+const {
+  createArticle,
+  getArticleById,
+  getArticleByName,
+  getArticles,
+  patchArticle,
+  deleteArticle,
+  sendGroupToDatabase,
+  // sendToDatabase,
+} = require('../controllers/articleController');
 
 // Adding an article to database
 // TODO: we need to add a group to the article and their categories
 // TODO: we need to add main idea property and general procedure
 router.post('/', createArticle);
 
-// WARN: This is for sending from local to remoteDatabase
-// Dont mess with this if u don't know
-// router.post('/add', sendToDatabase);
-
 // Recieving all articles stored
 router.get('/', getArticles);
 
 // Recieving an article by id
-router.get('/:id', getArticleById);
+router.get('/id/:id', getArticleById);
+router.get('/name/:name', getArticleByName);
 
 // Updating our articles data (PUT -- Update all the article, PATCH update part of an article)
 router.patch('/:id', patchArticle)
@@ -22,3 +28,9 @@ router.patch('/:id', patchArticle)
 // Delete an article by id
 router.delete('/:id', deleteArticle)
 module.exports = router;
+
+// WARN: This is for sending from local to remoteDatabase
+// Dont mess with this if u don't know
+// router.post('/add', sendToDatabase);
+router.post('/group/add', sendGroupToDatabase)
+
