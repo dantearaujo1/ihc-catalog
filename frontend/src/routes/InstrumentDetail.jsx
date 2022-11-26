986a6d0892275e01b366ddf2a2d88c049955f136
Dante Araújo <danteeng@hotmail.com>
2022-11-25 23:53:21 -0300

    Changing color titles and Using custom Button IHC

--- frontend/src/routes/InstrumentDetail.jsx
+++ frontend/src/routes/InstrumentDetail.jsx
@@ -2,9 +2,10 @@ import React, {useState} from 'react'
 import { useNavigate, useLocation } from "react-router-dom";
 
 import Stack from '@mui/material/Stack'
-import Button from '@mui/material/Button'
+import { IHCButtonRounded } from '../assets/ComponentStyle'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
+import { useTheme } from '@mui/material/styles'
 
 import NavigationHeader from '../components/Navigation/NavigationHeader'
 import NavigationBar from '../components/Navigation/NavigationBar'
@@ -18,6 +19,7 @@ import {
 const InstrumentDetail = () => {
   const navigate = useNavigate();
   const location = useLocation();
+  const theme = useTheme();
   const data = location.state.article;
   console.log(data);
 
@@ -31,15 +33,15 @@ const InstrumentDetail = () => {
             <Stack  direction='row' width="100%" height="100%">
               <Stack width="100%" height="auto" >
                 <Stack mt={5} >
-                  <Typography  variant="h6" sx={{fontWeight:"bold"}}>Main Idea</Typography>
+                  <Typography  color={theme.palette.primary.light} variant="h6" sx={{fontWeight:"bold"}}>Main Idea</Typography>
                   <Typography variant="p" mt={2}>{data?data.Article.main:"Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis."}</Typography>
                 </Stack>
                 <Stack mt={3} >
-                  <Typography variant="h6" sx={{fontWeight:"bold"}}>General Procedure</Typography>
+                  <Typography color={theme.palette.primary.light} variant="h6" sx={{fontWeight:"bold"}}>General Procedure</Typography>
                   <Typography variant="p" mt={2}>{data?data.Article.general:"Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis."}</Typography>
                 </Stack>
                 <Stack mt={3} >
-                  <Typography variant="h6" sx={{fontWeight:"bold"}}>Reference</Typography>
+                  <Typography variant="h6" color={theme.palette.primary.light} sx={{fontWeight:"bold"}}>Reference</Typography>
                   <Typography variant="p" mt={2}>{data?data.Article.reference:"Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis."}</Typography>
                 </Stack>
               </Stack>
@@ -47,10 +49,10 @@ const InstrumentDetail = () => {
                 <GroupBadge></GroupBadge>
               </Stack>
             </Stack>
-          <Button  variant='contained' onClick={() => {navigate(-1)}} sx={{mt: 5, mb: 6, borderRadius: 10}}>
+          <IHCButtonRounded  variant='contained'  onClick={() => {navigate(-1)}} sx={{mt: 5, mb: 6, borderRadius: 10}}>
             <FontAwesomeIcon  icon={faArrowLeftLong}/>
             <Typography ml={2}>Back</Typography>
-          </Button>
+          </IHCButtonRounded>
           <Stack height="40%" spacing={3}>
                 <Typography variant="h4">You may also be Interessed in ...</Typography>
                 <Stack direction="row"  spacing={2}>
