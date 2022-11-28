c71de5fde257ce834328cc604b9e2af10cd99d79
Dante Araújo <danteeng@hotmail.com>
2022-11-28 17:57:10 -0300

    Now Badge can move you to the results page by subcategory

--- frontend/src/components/GroupBadge.jsx
+++ frontend/src/components/GroupBadge.jsx
@@ -4,12 +4,27 @@ import Stack from '@mui/material/Stack'
 import { IHCButtonRounded } from '../assets/ComponentStyle'
 import Typography from '@mui/material/Typography'
 
+import { useNavigate } from "react-router-dom";
+
 const GroupBadge = ({category, subcategory}) => {
   const [data, setData] = useState({});
+  const navigate= useNavigate();
+
+  const handleClick = async () => {
+    const split = data.subcategory.split('/');
+    const joined = split.join('%2f');
+    const query = await fetch('/api/v1/article/sub/n/' + joined);
+    const toJson = await query.json();
+    console.log(toJson);
+    navigate('/result/' + toJson._id, {state: {lookedFor:[{category:{
+      id:toJson.categoryID,
+      selections:[{_id:toJson._id,name:data.subcategory,categoryID:toJson.categoryID}]
+    }}]}});
+  }
 
   useEffect( () => {
     setData({category:category, subcategory:subcategory});
-  }, [data] )
+  }, [] )
 
   return (
     <Stack spacing={1} mt={4} width="auto">
@@ -17,7 +32,7 @@ const GroupBadge = ({category, subcategory}) => {
         {data?data.category:"Category"}
       </Typography>
       <Stack direction="row" spacing={2}>
-        <IHCButtonRounded  variant="contained" sx={{width:"auto", borderRadius:10}}>
+        <IHCButtonRounded  onClick={handleClick} variant="contained" sx={{width:"auto", borderRadius:10}}>
           <Typography variant="caption">
             {data?data.subcategory:"SubCategory"}
           </Typography>
