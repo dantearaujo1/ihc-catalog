1f5dcedeab5ee011bc0c6356cfaeff8f13b937c5
Dante Araújo <danteeng@hotmail.com>
2022-11-28 15:23:25 -0300

    Started Creating our SideFilterComponent

---
+++ frontend/src/components/Filter/SideFilter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Stack from '@mui/material/Stack'
+import { IHCButtonRounded } from '../../assets/ComponentStyle'
+import Typography from '@mui/material/Typography'
+
+const SideFilter = () => {
+  return (
+    <Stack width="20%"  borderRight={2} >
+  Filter
+    </Stack>
+  )
+}
+
+export default SideFilter
