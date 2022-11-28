e115b753a1c725802ccb1a3b061bc075af403d79
Dante Araújo <danteeng@hotmail.com>
2022-11-28 15:24:50 -0300

    Correcting typography and recieving data from props

--- frontend/src/components/GroupBadge.jsx
+++ frontend/src/components/GroupBadge.jsx
@@ -1,19 +1,24 @@
-import React, {useState} from 'react'
+import React, { useState , useEffect } from 'react'
 
 import Stack from '@mui/material/Stack'
 import { IHCButtonRounded } from '../assets/ComponentStyle'
 import Typography from '@mui/material/Typography'
 
-const GroupBadge = () => {
-  const [data, setData] = useState();
+const GroupBadge = ({category, subcategory}) => {
+  const [data, setData] = useState({});
+
+  useEffect( () => {
+    setData({category:category, subcategory:subcategory});
+  }, [data] )
+
   return (
-    <Stack spacing={0.5} width="auto">
-      <Typography variant="h6">
+    <Stack spacing={1} mt={4} width="auto">
+      <Typography variant="inputLabel">
         {data?data.category:"Category"}
       </Typography>
       <Stack direction="row" spacing={2}>
         <IHCButtonRounded  variant="contained" sx={{width:"auto", borderRadius:10}}>
-          <Typography variant="buttonSmall">
+          <Typography variant="caption">
             {data?data.subcategory:"SubCategory"}
           </Typography>
         </IHCButtonRounded>
