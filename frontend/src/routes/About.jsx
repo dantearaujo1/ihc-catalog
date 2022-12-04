cd0d5fc692a20e53e3bbe5da819a3f4033195d3b
Dante Araújo <danteeng@hotmail.com>
2022-12-04 17:55:10 -0300

    Putting Ticianne Info from her article

--- frontend/src/routes/About.jsx
+++ frontend/src/routes/About.jsx
@@ -55,12 +55,23 @@ function About() {
       <NavigationHeader />
       <NavigationBar categories={cat} subcategories={sub} />
       <Stack alignItems='center'>
+        <Stack mt={4} spacing={3} width='80%'>
+          <Typography textAlign='justify' variant='h3'>
+            User Experience (UX) has been intensively investigated lately, resulting in the proposal of several evaluation instruments, methods, and techniques. However, the definition of UX and its constructs is still a work in progress, making User Experience a concept open to various interpretations. Consequently, the development of UX evaluation methods and instruments rely on very different assumptions, often making professionals and beginning researchers uncertain about choosing the right methods to evaluate user evaluation aspects.
+
+  Aiming to help fill in this gap, we present the results of a systematic snowballing procedure conducted to investigate the characteristics of the UX evaluation instruments that have been proposed and used by HCI community in the last years. We compiled information about 116 instruments aiming to assist researchers and practitioners in making informed choices about which instruments can support UX data collection, according to their research goals.
+          </Typography>
+
+          <Typography textAlign='justify' variant='h3'>
+  If you have any doubts or suggestions do not hesitate in contacting me at: ticianne@virtual.ufc.br"
+          </Typography>
+        </Stack>
         <Stack mt={4} spacing={1} alignItems='center'>
           <Typography variant='h1'>Our Team</Typography>
           <Typography variant='h3'>We're a group of people who helped our HIC Teacher to build an online catalog! </Typography>
           <Typography variant='h3'>We call ourselfs the Graúna Digital Team</Typography>
         </Stack>
-        <Stack direction="row" mb={8} mt={8} width='100%' height='100%'>
+        <Stack direction="row" mb={8} mt={8} width='80%' height='100%'>
           <Stack spacing={3} height='100%' width='100%' alignItems='center'>
             <Image height='auto' width="80%" showLoading  style={{aspectRatio: 1/1}} sx={{borderRadius:50}} src='../../people/dan.png'></Image>
             <Typography variant='h4' textAlign='center'>
@@ -107,10 +118,10 @@ function About() {
             </Typography>
           </Stack>
         </Stack>
-        <Stack height="100%">
-        </Stack>
       </Stack>
-      <IHCFooter></IHCFooter>
+      <Stack height="100%">
+        <IHCFooter></IHCFooter>
+      </Stack>
     </Stack>
 
   );
