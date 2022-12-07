444bbe84af2ba5df73441f5e8f9def140a98c993
Dante Araújo <danteeng@hotmail.com>
2022-12-07 12:12:09 -0300

    Correcting About Page

--- frontend/src/routes/About.jsx
+++ frontend/src/routes/About.jsx
@@ -55,65 +55,72 @@ function About() {
       <NavigationHeader />
       <NavigationBar categories={cat} subcategories={sub} />
       <Stack alignItems='center'>
-        <Stack mt={4} spacing={3} width='80%'>
-          <Typography textAlign='justify' variant='h3'>
-            User Experience (UX) has been intensively investigated lately, resulting in the proposal of several evaluation instruments, methods, and techniques. However, the definition of UX and its constructs is still a work in progress, making User Experience a concept open to various interpretations. Consequently, the development of UX evaluation methods and instruments rely on very different assumptions, often making professionals and beginning researchers uncertain about choosing the right methods to evaluate user evaluation aspects.
+        <Stack mt={10} spacing={3} width='80%'>
 
-  Aiming to help fill in this gap, we present the results of a systematic snowballing procedure conducted to investigate the characteristics of the UX evaluation instruments that have been proposed and used by HCI community in the last years. We compiled information about 116 instruments aiming to assist researchers and practitioners in making informed choices about which instruments can support UX data collection, according to their research goals.
+          <Typography textAlign='center' variant='h2'>
+            About the Project
           </Typography>
 
-          <Typography textAlign='justify' variant='h3'>
-  If you have any doubts or suggestions do not hesitate in contacting me at: ticianne@virtual.ufc.br"
+          <Typography textAlign='left' variant='subtitle1'>
+            User Experience (UX) has been intensively investigated lately, resulting in the proposal of several evaluation instruments, methods, and techniques. However, the definition of UX and its constructs is still a work in progress, making User Experience a concept open to various interpretations. Consequently, the development of UX evaluation methods and instruments rely on very different assumptions, often making professionals and beginning researchers uncertain about choosing the right methods to evaluate user evaluation aspects.
+          </Typography>
+
+          <Typography textAlign='left' variant='subtitle1'>
+            Aiming to help fill in this gap, we present the results of a systematic snowballing procedure conducted to investigate the characteristics of the UX evaluation instruments that have been proposed and used by HCI community in the last years. We compiled information about 116 instruments aiming to assist researchers and practitioners in making informed choices about which instruments can support UX data collection, according to their research goals.
+          </Typography>
+
+          <Typography textAlign='justify' variant='subtitle1'>
+            If you have any doubts or suggestions do not hesitate in contacting me at: ticianne@virtual.ufc.br
           </Typography>
         </Stack>
-        <Stack mt={4} spacing={1} alignItems='center'>
-          <Typography variant='h1'>Our Team</Typography>
-          <Typography variant='h3'>We're a group of people who helped our HIC Teacher to build an online catalog! </Typography>
-          <Typography variant='h3'>We call ourselfs the Graúna Digital Team</Typography>
+        <Stack mt={8} spacing={1} alignItems='center'>
+          <Typography variant='h2'>Our Team</Typography>
+          <Typography variant='subtitle1'>We're a group of people who helped our HIC Teacher to build an online catalog! </Typography>
+          <Typography variant='subtitle1'>We call ourselfs the Graúna Digital</Typography>
         </Stack>
         <Stack direction="row" mb={8} mt={8} width='80%' height='100%'>
           <Stack spacing={3} height='100%' width='100%' alignItems='center'>
-            <Image height='auto' width="80%" showLoading  style={{aspectRatio: 1/1}} sx={{borderRadius:50}} src='../../people/dan.png'></Image>
-            <Typography variant='h4' textAlign='center'>
+            <Image height='auto' width="80%" showLoading style={{ aspectRatio: 1 / 1 }} sx={{ borderRadius: 50 }} src='../../people/dan.png'></Image>
+            <Typography variant='h5' textAlign='center'>
               Dante de Araújo
             </Typography>
-            <Typography variant='h5' color='info.main' textAlign='center'>
-              Front and Back end Developer
+            <Typography variant='subtitle1' color='info.main' textAlign='center'>
+              Fullstack Developer
             </Typography>
           </Stack>
           <Stack spacing={3} height='100%' width='100%' alignItems='center'>
-              <Image height="auto" width="80%" showLoading  style={{aspectRatio: 1/1}} sx={{borderRadius:50}} src='../../people/Luis.jpg'></Image>
-            <Typography variant='h4' textAlign='center'>
+            <Image height="auto" width="80%" showLoading style={{ aspectRatio: 1 / 1 }} sx={{ borderRadius: 50 }} src='../../people/Luis.jpg'></Image>
+            <Typography variant='h5' textAlign='center'>
               Luís Eduardo
             </Typography>
-            <Typography variant='h5' color='info.main' textAlign='center'>
-              Documenter
+            <Typography variant='subtitle1' color='info.main' textAlign='center'>
+              Documenter - Designer Assistant
             </Typography>
           </Stack>
           <Stack spacing={3} height='100%' width='100%' alignItems='center'>
-            <Image height='auto' width="80%" showLoading  style={{aspectRatio: 1/1}} sx={{borderRadius:50}} src='../../people/Drielle.jpg'></Image>
-            <Typography variant='h4' textAlign='center'>
+            <Image height='auto' width="80%" showLoading style={{ aspectRatio: 1 / 1 }} sx={{ borderRadius: 50 }} src='../../people/Drielle.jpg'></Image>
+            <Typography variant='h5' textAlign='center'>
               Drielle Furtado
             </Typography>
-            <Typography variant='h5' color='info.main' textAlign='center'>
+            <Typography variant='subtitle1' color='info.main' textAlign='center'>
               Manager - Creative Leader - Designer
             </Typography>
           </Stack>
           <Stack spacing={3} height='100%' width='100%' alignItems='center'>
-            <Image height='auto' width="80%" showLoading  style={{aspectRatio: 1/1}} sx={{borderRadius:50}} src='../../people/Manoel.jpg'></Image>
-            <Typography variant='h4' textAlign='center'>
+            <Image height='auto' width="80%" showLoading style={{ aspectRatio: 1 / 1 }} sx={{ borderRadius: 50 }} src='../../people/Manoel.jpg'></Image>
+            <Typography variant='h5' textAlign='center'>
               Manoel Costa
             </Typography>
-            <Typography variant='h5' color='info.main' textAlign='center'>
+            <Typography variant='subtitle1' color='info.main' textAlign='center'>
               Documenter
             </Typography>
           </Stack>
           <Stack spacing={3} height='100%' width='100%' alignItems='center'>
-            <Image height='auto' width="80%" showLoading  style={{aspectRatio: 1/1}} sx={{borderRadius:50}} src='../../people/Max.jpg'></Image>
-            <Typography variant='h4' textAlign='center'>
+            <Image height='auto' width="80%" showLoading style={{ aspectRatio: 1 / 1 }} sx={{ borderRadius: 50 }} src='../../people/Max.jpg'></Image>
+            <Typography variant='subtitle1' textAlign='center'>
               Maxwell
             </Typography>
-            <Typography variant='h5' color='info.main' textAlign='center'>
+            <Typography variant='subtitle1' color='info.main' textAlign='center'>
               Developer - Requirements Planner
             </Typography>
           </Stack>
@@ -128,3 +135,4 @@ function About() {
 }
 
 export default About;
+
